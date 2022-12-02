55d7920d4e27eb2a75aa764c404d8724c9efa913
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-12-02 09:17:03 +0100

    ..

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
+echo "Downloading Spring Framework project"
+
 wget -q https://github.com/spring-projects/spring-framework/archive/refs/tags/v6.0.2.zip
+
+echo "Unzipping"
+
 unzip -q v6.0.2
 
 # create work dir
@@ -18,17 +23,25 @@ git add .gitignore
 git commit -m 'initialize repository'
 cd ..
 
+echo "Copying src files to work"
+
 # copy src files to work
 cp -r modules work/
+
+echo "Copying spring framework pages"
 
 # copy spring framework pages
 cp -r spring-framework-6.0.2/framework-docs/src/docs/asciidoc/ work/modules/ROOT/pages/
 
 tree work
 
+echo "Relocating images"
+
 # relocate images to match antora defaults
 mkdir -p work/modules/ROOT/images/images/
 mv work/modules/ROOT/pages/images/* work/modules/ROOT/images/images/
+
+echo "Copying antora config file"
 
 # copy antora config file
 cp antora.yml work/
